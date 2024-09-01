813336947b1225326e5f2d89a55abad754f09917
mrdapoyo <wiiuforever25@gmail.com>
2024-09-01 13:44:15 +0200

    start

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const app = express();
+app.use(express.static('public'));
+app.engine('art', require('express-art-template'));
+app.set("views", "./views");
+app.set('view engine', 'art');
+app.set('view options', {
+    debug: process.env.NODE_ENV !== 'production',
+    ignore: ['Math', 'Date', 'JSON', 'encodeURIComponent'],
+    minimize: false
+});
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// 404 Error Handler
+app.use((req, res, next) => {
+    res.status(404).send('404 Not Found');
+});
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
