43aff8a0ec9188ba842abded441b99d80fa84ce8
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-09-02 19:16:58 +0000

    a

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,22 +3,21 @@ const jwt = require("jsonwebtoken");
 const config = process.env;
 
 const verifyToken = async (req, res, next) => {
-    const token = await req.headers["x-access-token"];
-    console.log("Token received:", await token); // Log the received token
+    const token = req.cookies["x-access-token"];
+    console.log("Token received:", token); // Log the received token
 
     if (!token) {
         return res.status(403).send("A token is required for authentication");
     }
 
     try {
-        const decoded = jwt.verify(await token, config.TOKEN_KEY);
+        const decoded = jwt.verify(token, config.TOKEN_KEY);
         req.user = decoded;
+        next();
     } catch (err) {
         console.error("Token verification failed:", err); // Log the error
         return res.status(401).send("Invalid Token");
     }
-
-    return next();
 };
 
 module.exports = verifyToken;
