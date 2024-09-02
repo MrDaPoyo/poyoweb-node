59de6ff56bf60db471b9dc736f45c8c99687cdb2
mrdapoyo <wiiuforever25@gmail.com>
2024-09-02 20:08:52 +0200

    WORKS

--- auth.js
+++ auth.js
@@ -5,9 +5,10 @@ db = startDB.db;
 var authTokens = [];
 var bcrypt = require('bcrypt');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
 
 const authRouter = express.Router();
-
+authRouter.use(express.static('public'));
 // Middleware to parse request body
 authRouter.use(express.json());
 authRouter.use(bodyParser.urlencoded({ extended: true }));
@@ -50,10 +51,59 @@ authRouter.post('/register', async (req, res) => {
     }
 });
 
-// Logout route
-authRouter.post('/logout', (req, res) => {
-    // Implement your logout logic here
+authRouter.post("/login", async (req, res) => {
+    try {
+        // Get user input
+        const { username, password } = req.body;
+
+        // Validate user input
+        if (!(username && password)) {
+            return res.status(400).send("All input is required");
+        }
+
+        // Validate if user exists in our database
+        db.get('SELECT * FROM users WHERE username = ?;', [username], async (err, row) => {
+            if (err) {
+                console.error(err.message);
+                return res.status(500).send('Internal Server Error');
+            }
+
+            if (row) {
+                console.log(row);
+                const user = {
+                    id: row.id,
+                    email: row.email,
+                    password: row.password,
+                };
+                console.log(user);
+                // Compare passwords
+                const isPasswordValid = await bcrypt.compare(password, user.password);
+                if (isPasswordValid) {
+                    // Create token
+                    const token = jwt.sign(
+                        { user_id: user.id, username },
+                        process.env.TOKEN_KEY,
+                        {
+                            expiresIn: "5h",
+                        }
+                    );
+
+                    // Save token and respond with user info
+                    user.token = token;
+                    return res.status(200).json(user);
+                } else {
+                    return res.status(400).send("Invalid Credentials");
+                }
+            } else {
+                return res.status(400).send("Invalid Credentials");
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).send('Internal Server Error');
+    }
 });
+
 
 authRouter.get('/', (req, res) => {
     res.render('login', data = { title: 'Authentication Check', url: process.env.URL });
