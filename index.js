824cd6ca5434683ecf899e2b083478cd764ee04a
mrdapoyo <wiiuforever25@gmail.com>
2024-09-02 15:46:36 +0200

    sqlite

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const axios = require('axios');
 require('dotenv').config();
 
 const app = express();
@@ -13,13 +12,25 @@ app.set('view options', {
     minimize: false
 });
 
+const sqlite3 = require('sqlite3').verbose();
+
+// open the database
+let db = new sqlite3.Database('./poyoweb.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Connected to the chinook database.');
+});
+
+
+
 // Routes
 app.get('/', (req, res) => {
-    res.render('index', data= {title: 'Home'});
+    res.render('index', data = { title: 'Home', url: process.env.URL });
 });
 
 app.get('/login', (req, res) => {
-    res.render('login', data= {title: 'Login', token: Math.random().toString(36).substring(2)});
+    res.render('login', data = { title: 'Login', token: Math.random().toString(36).substring(2), url: process.env.URL });
 });
 
 
