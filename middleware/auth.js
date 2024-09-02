2ff3a3dd61c173260fb151acf675e1a5a5cb2dfe
mrdapoyo <wiiuforever25@gmail.com>
2024-09-02 20:29:55 +0200

    a

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,16 +2,16 @@ const jwt = require("jsonwebtoken");
 
 const config = process.env;
 
-const verifyToken = (req, res, next) => {
-    const token = req.body.token || req.query.token || req.headers["x-access-token"];
-    console.log("Token received:", token); // Log the received token
+const verifyToken = async (req, res, next) => {
+    const token = await req.headers["x-access-token"];
+    console.log("Token received:", await token); // Log the received token
 
     if (!token) {
         return res.status(403).send("A token is required for authentication");
     }
 
     try {
-        const decoded = jwt.verify(token, config.TOKEN_KEY);
+        const decoded = jwt.verify(await token, config.TOKEN_KEY);
         req.user = decoded;
     } catch (err) {
         console.error("Token verification failed:", err); // Log the error
