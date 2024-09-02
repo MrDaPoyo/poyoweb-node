31d5b3491ada4a0c806797e4d676b38fbdcb45b7
mrdapoyo <wiiuforever25@gmail.com>
2024-09-02 17:11:11 +0200

    start

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ let db = new sqlite3.Database('./poyoweb.db', sqlite3.OPEN_READWRITE, (err) => {
     if (err) {
         console.error(err.message);
     }
-    console.log('Connected to the chinook database.');
+    console.log('Connected to the poyoweb database.');
 });
 
 
--- startdb.js
+++ startdb.js
@@ -1,4 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
+const fs = require('fs');
 
 // Create a new database file if it doesn't exist
 const db = new sqlite3.Database('./poyoweb.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
@@ -16,5 +17,23 @@ db.run(`CREATE TABLE IF NOT EXISTS users (
     email TEXT NOT NULL
 )`);
 
+// Create websites table
+db.run(`CREATE TABLE IF NOT EXISTS websites (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    userID INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    FOREIGN KEY (userID) REFERENCES users(id)
+)`);
+
 // Insert a user into the table
-db.run(`INSERT INTO users (username, password) VALUES ('john', 'password123')`);
+db.run(`INSERT INTO users (username, password, email) VALUES ('test', 'test', 'a@example.com')`);
+
+// Print all users
+db.all(`SELECT * FROM users`, (err, rows) => {
+    if (err) {
+        console.error(err.message);
+    }
+    rows.forEach((row) => {
+        console.log(row);
+    });
+});
