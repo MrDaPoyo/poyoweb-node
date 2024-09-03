2cddb0c03c15f10ec2d9ed704dbdba2c569750d0
mrdapoyo <wiiuforever25@gmail.com>
2024-09-03 20:53:06 +0200

    better message

--- tokenSender.js
+++ tokenSender.js
@@ -22,7 +22,7 @@ function sendVerificationEmail(token, email) {
         subject: 'PoyoWeb - Email Verification',
 
         // This would be the text of email body
-        text: `Hi! You have recently registered on the PoyoWeb and entered your email. Please follow the given link to verify your email, and build a better web. ${process.env.URL}/auth/verify/${token} \n Thanks!\n--The PoyoWeb Team`
+        text: `Haii! :3\n You have recently registered on the PoyoWeb!\nPlease follow the given link to verify your email, and start buildig a better web. :D \n${process.env.URL}/auth/verify/${token}\n Thanks!\n--The PoyoWeb Team`
     };
 
     transporter.sendMail(mailConfigurations, function (error, info) {
