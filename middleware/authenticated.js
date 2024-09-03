ad4f74e8cd67ef48382174f32d989eaf0698d651
mrdapoyo <wiiuforever25@gmail.com>
2024-09-03 18:11:08 +0200

    FINALLY

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -5,17 +5,17 @@ const cookieParser = require("cookie-parser");
 const verifySessionToken = async (req, res, next) => {
     await cookieParser(req, res); // Parse the cookies
     var cookie = req.cookies; // Access the parsed cookies
-    try {
-        const token = cookie.sessionToken; // Assuming the token is stored in the 'sessionToken' cookie
-        // Perform any additional verification or checks here if needed
-    } catch (error) {
+    console.log(cookie);
+    if (cookie) { 
+    if (cookie["x-access-token"]) {
+        res.locals.loggedIn = true;
+        next();
+    } else {
         // Handle token verification error
-        console.error("Token verification failed:", error);
+        console.error("Token verification failed:");
         res.locals.loggedIn = false;
         next();
     }
-    res.locals.loggedIn = true;
-    next();
-};
+}};
 
 module.exports = verifySessionToken;
