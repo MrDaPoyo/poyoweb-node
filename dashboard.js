ca1f8a6cfc9a56597eb9834b120316e653a98a98
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-09-05 17:29:26 +0000

    full working dashboard :3

--- dashboard.js
+++ dashboard.js
@@ -10,32 +10,45 @@ router.get('/', async (req, res) => {
         if (dirPath.includes("..")) {
             res.send("HA! Good try, Hacker :3", 404);
         } else {
-        const results = await dirWalker("websites/users/" + await req.user.username, dirPath);
-        const pastDir = path.relative("websites/users/" + req.user.username, path.dirname(dirPath));
-        const cleanPath = path.relative("websites/users/" + req.user.username, dirPath);
+            const results = await dirWalker("websites/users/" + await req.user.username, dirPath);
+            const pastDir = path.relative("websites/users/" + req.user.username, path.dirname(dirPath));
+            const cleanPath = path.relative("websites/users/" + req.user.username, dirPath);
 
-        res.render('dashboard', { files: await results, past: pastDir, cleanPath, dashboard: (dirPath == "websites/users/" + await req.user.username) });
+            res.render('dashboard', { files: await results, past: pastDir, cleanPath, dashboard: (dirPath == "websites/users/" + await req.user.username) });
         }
     } catch (err) {
         res.send("Directory Not Found. Good try, Hacker :3");
     }
 });
 
-router.post('/removeFolder/', (req, res) => {
+router.get('/remove', (req, res) => {
     try {
-        fs.unlinkSync("websites/users/" + req.user.username + "/" + req.body.cleanPath);
-        res.redirect('/dashboard');
+        fs.unlinkSync("websites/users/" + req.user.username + "/" + req.query.dir);
+        res.redirect(`/dashboard?dir=${path.dirname(req.query.dir)}`);
     } catch (err) {
-        throw err;
+        fs.rmdirSync("websites/users/" + req.user.username + "/" + req.query.dir, { recursive: true });
     }
 });
 
 router.post('/create', (req, res) => {
     try {
-        fs.writeFileSync(req.body.path, "");
-        res.redirect('/dashboard/'+req.body.dir);
+        var dirname = "websites/users/" + req.user.username + "/" + req.query.dir
+        if (dirname.includes("..")) {
+            res.send("HA! Good try, Hacker :3", 404);
+        } else if (!fs.existsSync(dirname)) {
+            dirname.replace(/\\/g, "/");
+            if (dirname.includes(".")) {
+                fs.writeFileSync(dirname, "");
+            } else {
+                fs.mkdirSync(dirname);
+                res.redirect('/dashboard/?dir=' + req.body.dir);
+            }
+        } else {
+            res.send("File/Directory already exists.");
+        }
     } catch (err) {
-        throw err;
+        console.log(err);
+        res.send("Error creating file/directory.");
     }
 });
 
