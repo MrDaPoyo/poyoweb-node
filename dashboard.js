b504deeb945194ceba01046eb9cd511c77295195
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-09-05 15:35:57 +0000

    go back option!

--- dashboard.js
+++ dashboard.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
+const path = require('path');
 const dirWalker = require('./snippets/dirWalker');
 
 router.get('/', async (req, res) => {
     try {
-        const dirPath = "websites/users/" + req.user.username + (req.query.dir ? "/" + req.query.dir : "");
-        const results = await dirWalker("websites/users/" + req.user.username, dirPath);
-        res.render('dashboard', { files: await results });
+        const dirPath = "websites/users/" + await req.user.username + (req.query.dir ? "/" + req.query.dir : "");
+        if (dirPath.includes("..")) {
+            res.send("HA! Good try, Hacker :3", 404);
+        } else {
+        const results = await dirWalker("websites/users/" + await req.user.username, dirPath);
+        const pastDir = path.relative("websites/users/" + req.user.username, path.dirname(dirPath));
+        res.render('dashboard', { files: await results, past: pastDir, dashboard: (dirPath == "websites/users/" + await req.user.username) });
+        }
     } catch (err) {
         res.send("Directory Not Found. Good try, Hacker :3");
     }
