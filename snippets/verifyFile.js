51d1d273b571b83e165e33923e7e7c88d7df8b1e
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-09-07 13:47:26 +0000

    added php

--- snippets/verifyFile.js
+++ snippets/verifyFile.js
@@ -1,9 +1,9 @@
 var VALID_EXTENSIONS = [
-    'html', 'htm', 'txt', 'text', 'css', 'js', 'jpg', 'jpeg', 'png', 'apng', 'gif', 'svg', 'md', 'markdown', 'eot', 'ttf', 'woff', 'woff2', 'json', 'geojson', 'csv', 'tsv', 'mf', 'ico', 'pdf', 'asc', 'key', 'pgp', 'xml', 'mid', 'midi', 'manifest', 'otf', 'webapp', 'less', 'sass', 'rss', 'kml', 'dae', 'obj', 'mtl', 'scss', 'webp', 'avif', 'xcf', 'epub', 'gltf', 'bin', 'webmanifest', 'knowl', 'atom', 'opml', 'rdf', 'map', 'gpg', 'resolveHandle', 'pls', 'yaml', 'yml', 'toml', 'osdx', 'mjs', 'cjs', 'ts', 'glb', 'py'
+    'html', 'htm', 'txt', 'text', 'css', 'js', 'jpg', 'jpeg', 'png', 'apng', 'gif', 'svg', 'md', 'markdown', 'eot', 'ttf', 'woff', 'woff2', 'json', 'geojson', 'csv', 'tsv', 'mf', 'ico', 'pdf', 'asc', 'key', 'pgp', 'xml', 'mid', 'midi', 'manifest', 'otf', 'webapp', 'less', 'sass', 'rss', 'kml', 'dae', 'obj', 'mtl', 'scss', 'webp', 'avif', 'xcf', 'epub', 'gltf', 'bin', 'webmanifest', 'knowl', 'atom', 'opml', 'rdf', 'map', 'gpg', 'resolveHandle', 'pls', 'yaml', 'yml', 'toml', 'osdx', 'mjs', 'cjs', 'ts', 'glb', 'py', 'php'
 ];
 
 var VALID_EDITABLE_EXTENSIONS = [
-    'html', 'htm', 'txt', 'js', 'css', 'scss', 'md', 'markdown', 'manifest', 'less', 'webmanifest', 'xml', 'json', 'opml', 'rdf', 'svg', 'gpg', 'pgp', 'resolveHandle', 'pls', 'yaml', 'yml', 'toml', 'osdx', 'mjs', 'cjs', 'ts',
+    'html', 'htm', 'txt', 'js', 'css', 'scss', 'md', 'markdown', 'manifest', 'less', 'webmanifest', 'xml', 'json', 'opml', 'rdf', 'svg', 'gpg', 'pgp', 'resolveHandle', 'pls', 'yaml', 'yml', 'toml', 'osdx', 'mjs', 'cjs', 'ts', 'php',
 ];
 
 function checkFileName(name) {
