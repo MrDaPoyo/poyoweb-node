3ee628e262fc216e4b436c2a4e0a9747f4a003ae
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-10-05 16:18:17 +0200

    Update domains.js

--- domains.js
+++ domains.js
@@ -5,25 +5,33 @@ const startDB = require('./startdb');
 const router = express.Router();
 
 router.use(async (req, res, next) => {
-    const subdomain = req.headers.host.split('.')[0];
+    const host = req.headers.host;
 
-    if (!subdomain.includes('localhost') && subdomain !== 'www') {
-        // var views = (await startDB.retrieveViews(subdomain)).views;
-        // console.log("hit!: "+subdomain + ", views: " + await views);
+    // Check if the host matches the expected domain structure
+    const expectedDomain = 'poyoweb.poyo.study';
+    const subdomain = host.split('.')[0];
+
+    if (host.endsWith(expectedDomain) && subdomain !== 'www') {
         res.locals.isPoyoweb = false;
         const subdomainPath = path.join(__dirname, 'websites/users/', subdomain);
-        express.static(subdomainPath)(req, res, next);
+
+        // Increment view count for the subdomain
         startDB.addView(subdomain);
+
         try {
+            // Serve the index.html file
             res.sendFile(path.join(subdomainPath, 'index.html'));
         } catch (err) {
             try {
+                // If index.html is not found, serve the 404.html file
                 res.sendFile(path.join(subdomainPath, '404.html'));
             } catch (err) {
+                // If neither file is found, respond with a 404 status
                 res.status(404).send('Website not found');
             }
         }
     } else {
+        // For other requests, continue to the next middleware
         res.locals.isPoyoweb = true;
         next();
     }
