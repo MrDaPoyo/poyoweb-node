a42d930a1e8e5c4be404aea2ab40ee378f3810b2
MrDaPoyo <wiiuforever25@gmail.com>
2024-10-07 08:45:34 +0300

    Finally fixed the middleware

--- middleware/verified.js
+++ middleware/verified.js
@@ -2,16 +2,25 @@ var startDB = require("../startdb");
 const loggedIn = require("../verifyJwt");
 
 const redirectIfNotVerified = async (req, res, next) => {
-    let user = await loggedIn(req, res, next);
-    if (user["email"]) {
-        user = await startDB.findUserByEmail(await user["email"]);
-        if (user["verified"] === 1) {
-            next();
+    try {
+        let user = await loggedIn(req, res, next);
+
+        if (user["email"]) {
+            user = await startDB.findUserByEmail(user["email"]);
+
+            if (user["verified"] == 1) {
+                return next();
+            } else {
+                res.clearCookie('token');  // Remove the token
+                return res.redirect('/');
+            }
         } else {
-            res.redirect('/');
+            res.clearCookie('token');  // Remove the token
+            return res.redirect('/');
         }
-    } else {
-        res.redirect('/');
+    } catch (error) {
+        res.clearCookie('token');  // Remove the token
+        return res.redirect('/');
     }
 }
 
