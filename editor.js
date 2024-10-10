931806852a709081bebf9be60207ab9c55488aa1
MrDaPoyo <wiiuforever25@gmail.com>
2024-10-10 09:55:31 +0300

    Fixed editor.js + file sanitising

--- editor.js
+++ editor.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const fs = require('fs');
+const fs = require('fs').promises;
+const path = require('path');
 const bodyParser = require('body-parser');
 var router = express.Router();
 require('dotenv').config();
@@ -8,35 +9,55 @@ router.use(bodyParser.urlencoded({ extended: true }));
 
 
 router.get('/', async function (req, res) {
-    const dirPath = req.query.file ? "/" + req.query.file : "";
-    if (dirPath.includes("..")) {
-        res.send("HA! Good try, Hacker :3", 404);
-    } else {
-        fs.readFile(`websites/users/${req.user.username}/${dirPath}`, 'utf8', function (err, data) {
-            if (err) {
-                console.log(err);
-                res.send('An error occurred');
-            } else {
-                console.log(data);
-                res.render("editor", { fileName: req.query.file, fileData: data, url: process.env.URL });
-            }
-        });
+	if (req.query.file) {
+    	const dirPath = req.query.file ? "/" + req.query.file : "";
+   	 	if (dirPath.includes("..") || !dirPath.includes(".")) {
+        	res.send("HA! Good try, Hacker :3", 404);
+    	}	else {
+    		try {
+    		await fs.access(`websites/users/${req.user.username}/${dirPath}`);
+       		 fs.readFile(`websites/users/${req.user.username}/${dirPath}`, 'utf8', function (err, data) {
+            	if (err) {
+        	        console.log(err);
+    	            res.send('An error occurred');
+	            } else {
+                	console.log(data);
+                	res.render("editor", { fileName: req.query.file, fileData: data, url: process.env.URL });
+            	}
+       		 });
+       		 } catch (err) {
+       		 	res.send("File not found :P");
+       		 }
+   		 }
+    }
+    else {
+    	res.send("No file selected :P")
     }
 });
 
 router.post('/save', async function (req, res, next) {
-    const filename = req.query.q;
-    const filePath = `websites/users/${await req.user.username}/${filename}`;
-    const fileContent = await req.body.code;
+    try {
+        const filename = req.query.q;
+        const basePath = `websites/users/${req.user.username}`;
+        const filePath = path.join(basePath, filename); // Construct the file path
+        const fileContent = req.body.code;
 
-    fs.writeFile(filePath, fileContent, 'utf8', function (err) {
-        if (err) {
-            console.log(err);
-            res.status(500).send('An error occurred while saving the file');
-        } else {
+        // Check if the file already exists
+        try {
+            await fs.access(filePath); // If no error, the file exists
+            await fs.writeFile(filePath, fileContent, 'utf8');
             res.redirect(`/editor/?file=${filename}`);
+            
+        } catch (err) {
+            if (err.code !== 'ENOENT') { // ENOENT means file does not exist, so we can proceed
+                throw err; // Re-throw other errors
+            }
         }
-    });
+        
+    } catch (err) {
+        console.error('Error saving file:', err);
+        res.status(500).send('An error occurred while saving the file');
+    }
 });
 
 router.post('/delete', async function (req, res, next) {
