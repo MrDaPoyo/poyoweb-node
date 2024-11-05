9a02c667cd6fa954b082a11ff84d09b5e66be055
MrDaPoyo <wiiuforever25@gmail.com>
2024-11-05 10:47:38 +0200

    prevent custom domains from getting rewritten + readme

--- fixdomains.js
+++ fixdomains.js
@@ -1,5 +1,13 @@
 const sqlite3 = require('sqlite3').verbose();
 
+// Get the old domain from command-line arguments
+const oldDomain = process.argv[2];
+
+if (!oldDomain) {
+  console.error('Please provide the old domain as an argument.');
+  process.exit(1);
+}
+
 // Connect to the SQLite database
 const db = new sqlite3.Database('poyoweb.db', (err) => {
   if (err) {
@@ -11,9 +19,13 @@ const db = new sqlite3.Database('poyoweb.db', (err) => {
 
 // Function to update the domain column
 const updateDomains = () => {
-  const query = `UPDATE websites SET domain = name || '.poyoweb.me'`;
+  const query = `
+    UPDATE websites
+    SET domain = name || '.poyoweb.me'
+    WHERE domain LIKE '%' || ? || '%'
+  `;
 
-  db.run(query, function (err) {
+  db.run(query, [oldDomain], function (err) {
     if (err) {
       console.error('Error updating domains:', err.message);
     } else {
